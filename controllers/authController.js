c5f49c914d74305592225e8e47069f4e32a43024
AR-JUN-G <arjun07091102@gmail.com>
2025-06-26 22:08:03 +0530

    #260625-Cors issue fix and log added

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,11 +6,15 @@ exports.register = async (req, res) => {
   const { name, email, password, role } = req.body;
   try {
     const exist = await User.findOne({ email });
-    if (exist) return res.status(400).json({ msg: 'User already exists' });
+    if (exist){
+      console.log("User Already exists");
+      return res.status(400).json({ msg: 'User already exists' });
+    } 
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ name, email, password: hashedPassword, role: role || 'user' });
     await newUser.save();
+    console.log("User Added Successfully");
     res.status(201).json({ msg: 'Registered successfully' });
   } catch (err) {
     res.status(500).json({ msg: err.message });
@@ -21,10 +25,16 @@ exports.login = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ msg: 'User not found' });
+    if (!user) {
+      console.log("Invalid User");
+      return res.status(400).json({ msg: 'User not found' });
+    }
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: 'Invalid password' });
+    if (!isMatch){
+      console.log("Login Failed MisMatched Password");
+      return res.status(400).json({ msg: 'Invalid password' });
+    } 
 
     const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1d' });
     res.json({
